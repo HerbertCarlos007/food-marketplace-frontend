2cc9296e348204291993fd3aff183930f5c428a5
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-09-11 01:47:48 -0300

    feat: Adicionando icone na sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-
+  faChevronRight = faChevronRight;
 }
