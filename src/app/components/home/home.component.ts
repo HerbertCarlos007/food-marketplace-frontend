f6e0257e33b5d4b4e2a8c1cece97a7dd7f62737d
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-09-11 03:40:02 -0300

    feat: Criando componente de carrinho

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { HeaderComponent } from '../header/header.component';
+import { ProductsComponent } from '../products/products.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SidebarComponent, HeaderComponent],
+  imports: [SidebarComponent, HeaderComponent, ProductsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
