b451b7fcac12d05ef8102054a2624dfcdb9abee5
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-09-13 14:53:24 -0300

    feat: Criando componente configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, ConfigurationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
 
 export const routes: Routes = [
     {path: '', component: LoginComponent},
-    {path: 'home', component: HomeComponent}
+    {path: 'home', component: HomeComponent},
+    {path: 'configuration', component: ConfigurationComponent}
 ];
