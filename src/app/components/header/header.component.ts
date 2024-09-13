621e724542331c9de2d01ca25a0ef5f38c6c44a7
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-09-13 14:53:58 -0300

    feat: Adicionando icone e navegação para pagina de configuração

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faHouse } from '@fortawesome/free-solid-svg-icons';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faGear } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
@@ -15,4 +17,6 @@ export class HeaderComponent {
   faHouse = faHouse
   faCartShopping = faCartShopping
   faMagnifyingGlass = faMagnifyingGlass
+  faGear = faGear
+
 }
