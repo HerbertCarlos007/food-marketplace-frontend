b40d7e3341371ae6154dd884b3d2bb57d30ca6aa
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-09-17 22:41:21 -0300

    feat: Usando modal no componente product management

--- src/app/components/product-management/product-management.component.ts
+++ src/app/components/product-management/product-management.component.ts
@@ -4,11 +4,12 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import { faPencil } from '@fortawesome/free-solid-svg-icons';
 import { faFilter } from '@fortawesome/free-solid-svg-icons';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-product-management',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, ModalComponent],
   templateUrl: './product-management.component.html',
   styleUrl: './product-management.component.css'
 })
@@ -17,4 +18,14 @@ export class ProductManagementComponent {
   faTrashCan = faTrashCan
   faPencil = faPencil
   faFilter = faFilter
+
+  isModalOpen: boolean = false
+
+  setOpenModal() {
+    this.isModalOpen = !this.isModalOpen
+  }
+  
+  setCloseModal(): void {
+    this.isModalOpen = !this.isModalOpen
+  }
 }
