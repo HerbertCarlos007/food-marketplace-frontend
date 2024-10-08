34d56d4c47c6b6fbf7f3c1639f4e9e9cffd35bf1
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-10-08 02:03:00 -0300

    feat: Criando tela de não autorizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { ConfigurationComponent } from './components/configuration/configuration.component';
 import { AuthGuard } from './guards/auth.guard';
+import { UnauthorizedComponent } from './components/unauthorized/unauthorized.component';
 
 export const routes: Routes = [
     {path: '', component: LoginComponent},
     {path: 'home', component: HomeComponent},
-    {path: 'configuration', component: ConfigurationComponent, canActivate: [AuthGuard] }
+    {path: 'configuration', component: ConfigurationComponent, canActivate: [AuthGuard]},
+    {path: 'unauthorized', component: UnauthorizedComponent}
 ];
