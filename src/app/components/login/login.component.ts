4f5f86c5a7bf90415dc092627d6ac4c008afd25e
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-10-24 18:43:24 -0300

    feat: Criando validações para o form de login e registro

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,26 +1,36 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { LoginService } from '../../services/login.service';
 import { User } from '../../interfaces/user';
-import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule,],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   providers: [LoginService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup
+  registerForm: FormGroup
   isLoginForm = true
-  email: string = ''
-  name: string = ''
-  password: string = ''
+  
 
+  constructor(private loginService: LoginService, private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['',[Validators.required, Validators.minLength(6)]]
+    })
 
-  constructor(private loginService: LoginService) {}
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['',[Validators.required, Validators.minLength(6)]]
+    })
+    
+  }
 
   ngOnInit() {
     const subdomain = window.location.hostname.split(".")[0] === 'localhost' ? 'dev' : window.location.hostname.split(".")[0]
@@ -31,21 +41,61 @@ export class LoginComponent {
     this.isLoginForm = !this.isLoginForm;
   }
 
+  getErrorLoginForm(controlName: string): string | null {
+    const control = this.loginForm.get(controlName)
+  
+    if (control?.invalid && control?.touched) {
+      if (control.errors?.['email']) {
+        return 'E-mail inválido.';
+      }
+      if (control.errors?.['minlength']) {
+        return `A senha deve ter pelo menos 6 caracteres.`
+      }
+    }
+    
+    return null
+  }
+
+  getErrorRegisterForm(controlName: string): string | null {
+    const control = this.registerForm.get(controlName)
+  
+    if (control?.invalid && control?.touched) {
+      if (control.errors?.['required']) {
+        return 'Este campo é obrigatório.';
+      }
+      if (control.errors?.['email']) {
+        return 'E-mail inválido.';
+      }
+      if (control.errors?.['minlength']) {
+        return `A senha deve ter pelo menos 6 caracteres.`
+      }
+    }
+    
+    return null
+  }
 
   login() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+  
     const user: User = {
-      email: this.email,
-      password: this.password
+      email: this.loginForm.get('email')?.value,
+      password: this.loginForm.get('password')?.value
     };
-    this.loginService.login(user)
+    this.loginService.login(user);
   }
 
   register() {
+    if (this.registerForm.invalid) {
+      return;
+    }
+  
     const user: User = {
-      name: this.name,
-      email: this.email,
-      password: this.password
+      name: this.registerForm.get('name')?.value,
+      email: this.registerForm.get('email')?.value,
+      password: this.registerForm.get('password')?.value
     };
-    this.loginService.register(user)
+    this.loginService.register(user);
   }
 }
