0781f02bea88b9fc2f8f1d246251c12d62360bae
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-11-20 17:49:13 -0300

    feat: Retornando as categorias na tabelas

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -4,16 +4,23 @@ import { LoginService } from '../../services/login.service';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Category } from '../../interfaces/category';
 import { CommonModule } from '@angular/common';
+import { faPencil } from '@fortawesome/free-solid-svg-icons';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, FontAwesomeModule],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css',
 })
 export class CategoriesComponent {
   categoryForm: FormGroup
+  categories: Category[] = []
+
+  faTrashCan = faTrashCan;
+  faPencil = faPencil;
 
   constructor(private categoryService: CategoriesService, private loginService: LoginService, private fb: FormBuilder) {
     this.categoryForm = this.fb.group({
@@ -22,7 +29,18 @@ export class CategoriesComponent {
   }
 
   ngOnInit(): void {
-    
+    this.getAllCategories()
+  }
+
+  getAllCategories() {
+    this.categoryService.getCategories().subscribe({
+      next: (response) => {
+        this.categories = response
+      },
+      error: (error) => {
+        console.error('Erro ao obter categorias:', error);
+      }
+    });
   }
 
   createCategory() {
@@ -32,8 +50,6 @@ export class CategoriesComponent {
       console.error('Store ID não encontrado!');
       return;
     }
-
-    console.log('olaa', this.categoryForm.get('name')?.value)
     
     const category: Category = {
       name: this.categoryForm.get('name')?.value,
