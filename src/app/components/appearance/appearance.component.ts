2db3ac729279ecdffa2e3274392e6478960e37bc
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-11-28 20:42:25 -0300

    feat: Criando tela de aparencia

---
+++ src/app/components/appearance/appearance.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appearance',
+  standalone: true,
+  imports: [],
+  templateUrl: './appearance.component.html',
+  styleUrl: './appearance.component.css'
+})
+export class AppearanceComponent {
+
+}
--- src/app/components/configuration/configuration.component.ts
+++ src/app/components/configuration/configuration.component.ts
@@ -4,11 +4,12 @@ import { ProductManagementComponent } from '../product-management/product-manage
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from '../users/users.component';
 import { CategoriesComponent } from "../categories/categories.component";
+import { AppearanceComponent } from '../appearance/appearance.component';
 
 @Component({
   selector: 'app-configuration',
   standalone: true,
-  imports: [CommonModule, ConfigurationSidebarComponent, ProductManagementComponent, UsersComponent, CategoriesComponent, CategoriesComponent],
+  imports: [CommonModule, ConfigurationSidebarComponent, ProductManagementComponent, UsersComponent, CategoriesComponent, CategoriesComponent, AppearanceComponent],
   templateUrl: './configuration.component.html',
   styleUrl: './configuration.component.css'
 })
