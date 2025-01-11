0e326c76bda528df904e6b0d3bb45e5d0a61e988
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2025-01-11 16:36:42 -0300

    feat: Adicionando funcao de atualizar usuario

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -6,13 +6,25 @@ import { User } from '../../interfaces/user';
 import { CommonModule } from '@angular/common';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { ModalComponent } from '../modal/modal.component';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [FontAwesomeModule, CommonModule, ModalComponent, FormsModule, ReactiveFormsModule],
+  imports: [
+    FontAwesomeModule,
+    CommonModule,
+    ModalComponent,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css',
 })
@@ -21,29 +33,50 @@ export class UsersComponent {
   faPlus = faPlus;
   users: User[] = [];
 
-   userForm: FormGroup
+  userForm: FormGroup;
 
   isModalOpen: boolean = false;
   isModalCreateUserOpen: boolean = false;
 
-  constructor(private userService: UsersService, private loginService: LoginService, private fb: FormBuilder) {
-     this.userForm = fb.group({
-       name: ['', Validators.required],
-       email: ['', Validators.required],
-       password: ['', Validators.required],
-     });
+  id: string = '';
+
+  constructor(
+    private userService: UsersService,
+    private loginService: LoginService,
+    private fb: FormBuilder
+  ) {
+    this.userForm = fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      status: ['', Validators.required],
+      role: ['', Validators.required],
+    });
   }
 
   ngOnInit(): void {
-    this.getAllUsers()
+    this.getAllUsers();
+  }
+
+  setOpenModal(user?: User) {
+    this.isModalOpen = true;
+    this.id = user ? String(user.id) : '';
+    this.userForm.patchValue({
+      name: user?.name,
+      email: user?.email,
+      status: user?.status,
+      role: user?.role,
+    });
+
+    console.log(this.id);
   }
 
   setCloseModal(): void {
     this.isModalOpen = !this.isModalOpen;
   }
 
-  setCloseModalCreateUser(){
-    this.isModalCreateUserOpen = !this.isModalCreateUserOpen
+  setCloseModalCreateUser() {
+    this.isModalCreateUserOpen = !this.isModalCreateUserOpen;
   }
 
   getAllUsers() {
@@ -65,7 +98,6 @@ export class UsersComponent {
       return;
     }
 
-
     const user: User = {
       name: this.userForm.get('name')?.value,
       email: this.userForm.get('email')?.value,
@@ -75,5 +107,32 @@ export class UsersComponent {
       password: this.userForm.get('password')?.value,
     };
     this.loginService.register(user);
+  }
+
+  update() {
+    const storeId = this.loginService.getStoreId();
+
+    if (!storeId) {
+      console.error('Store ID não encontrado!');
+      return;
+    }
+
+    const user: User = {
+      name: this.userForm.get('name')?.value,
+      email: this.userForm.get('email')?.value,
+      status: this.userForm.get('status')?.value,
+      role: this.userForm.get('role')?.value,
+      storeId: storeId,
+    };
+
+    this.userService.update(user, this.id).subscribe({
+      next: () => {
+        this.getAllUsers();
+        this.setCloseModal();
+      },
+      error: (error) => {
+        console.error('Erro ao atualizar o usuário:', error);
+      },
+    });
   }
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -5,22 +5,27 @@ import { User } from '../interfaces/user';
 import { map, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
   private baseApiUrl = environment.baseApiUrl;
-  role = localStorage.getItem('role') || ''
-  
-  constructor(private http: HttpClient) { }
+  role = localStorage.getItem('role') || '';
+
+  constructor(private http: HttpClient) {}
 
   getAllUsers(): Observable<User[]> {
     const apiUrl = `${this.baseApiUrl}api/user`;
-    return this.http.get<User[]>(apiUrl).pipe(
-      map((response: User[]) => response)
-    )
+    return this.http
+      .get<User[]>(apiUrl)
+      .pipe(map((response: User[]) => response));
   }
 
   getUserRole(): string {
-    return this.role
+    return this.role;
+  }
+
+  update(user: User, id: string): Observable<User> {
+    const apiUrl = `${this.baseApiUrl}auth/update/${id}`;
+    return this.http.put<User>(apiUrl, user);
   }
 }
